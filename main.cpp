a3822e624da7a13b024465d27f1bda1becb52805
400cs <bennguyenhb@gmail.com>
2022-01-15 15:42:06 -0800

    new function for intro inputs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
-
+#include <string>
 using namespace std;
+
+string player1NAME;
+string player2NAME;
+
+void introLines()
+{
+    cout << "Enter your name player 1" << endl;
+    getline(cin, player1NAME);
+    cout << "Enter your name player 2" << endl;
+    getline(cin, player2NAME);
+    cout << player1NAME << " will have the first move has X." << endl;
+    cout << player2NAME << " will have the next move has O." << endl;
+}
+
+
 
 int main()
 {
+    introLines();
     cout << "    |    |    " << endl;
     cout << "    |    |    " << endl;
     cout << "____|____|____" << endl;
@@ -14,5 +30,8 @@ int main()
     cout << "    |    |    " << endl;
     cout << "    |    |    " << endl;
 
-    return 0;
+    
+    
+    
+    
 }
